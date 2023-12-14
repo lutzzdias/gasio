9a9f699453e4d583d0ea93a84405a43f55e86a42
lutzzdias <thiagolutzdias@hotmail.com>
2023-12-14 15:38:23 +0000

    Initial Commit

---
+++ Gasio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gasio
+//
+//  Created by Thiago Lütz Dias on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gasio/GasioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasioApp.swift
+//  Gasio
+//
+//  Created by Thiago Lütz Dias on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GasioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
