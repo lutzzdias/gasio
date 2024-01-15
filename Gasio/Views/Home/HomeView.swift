54c83321065b4c26064d98f1a0e8024b0927db7a
lutzzdias <thiagolutzdias@hotmail.com>
2024-01-15 09:02:21 +0000

    feat: initial graph for home screen

--- Gasio/Views/Home/HomeView.swift
+++ Gasio/Views/Home/HomeView.swift
@@ -6,13 +6,34 @@
 //
 
 import SwiftUI
+import SwiftData
+import Charts
 
 struct HomeView: View {
     @AppStorage("username") var username: String = ""
+    @Query(sort: [SortDescriptor(\FuelEntry.date, order: .reverse)]) private var entries: [FuelEntry]
+    
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("Welcome")
+                .font(.title)
+            
             Text(username)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Chart {
+                ForEach(entries) { entry in
+                    BarMark(x: .value("Date", entry.date),
+                            y: .value("Consumption", entry.consumption))
+                    .position(by: .value("Consumption", entry.consumption))
+                }
+            }
+            .frame(maxHeight: 300)
+            .padding()
+            
+            Spacer()
         }
         .padding()
     }
